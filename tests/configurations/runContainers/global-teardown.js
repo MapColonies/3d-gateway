ee9b3f7b54cbc911c6cf4dcb9754ad319b4a3f3f
netanelC <netanel153@gmail.com>
2024-05-16 15:14:53 +0300

    fix: tests and docker-compose

--- tests/configurations/runContainers/global-teardown.js
+++ tests/configurations/runContainers/global-teardown.js
@@ -1,6 +1,6 @@
 // global-teardown.js - clean-up after all tests
-const dockerCompose = require('docker-compose');
+const dockerCompose = require('docker-compose/dist/v2');
 
 module.exports = async () => {
-  dockerCompose.down();
+  await dockerCompose.down();
 };
